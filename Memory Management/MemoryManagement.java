5f7e46c9c13309b48148c149021289539830ad52
AmirrudinISM <amirrudin_ism@outlook.my>
2020-09-12 02:02:26 +0800

    completed implementation of Next fit & Best fit

--- Memory Management/MemoryManagement.java
+++ Memory Management/MemoryManagement.java
@@ -25,57 +25,73 @@ class MemoryManagement{
 	public static void main(String[] args) {
     	Scanner input = new Scanner(System.in);
 		
-		//prompt & get processes
+		//creates processes & prompts for size of each process
 		int procCount = 0;
-    	System.out.print("Enter number of processes: ");
+    	System.out.println("Enter number of processes: ");
     	procCount = input.nextInt();
     	Process [] processInst = processInitializer(procCount, input);
     	
-    	//promt & get memory blocks
+    	//creates memory blocks & prompts size of each memory block
     	int blockCount = 0;
-    	System.out.print("Enter number of memory blocks: ");
+    	System.out.println("Enter number of memory blocks: ");
     	blockCount = input.nextInt();
     	MemoryBlock [] memoryBlockInst = memoryBlockInitializer(blockCount, input);
     	
-		//array list forming final list of partitioned memory
-		ArrayList <Process> partition = firstFit(processInst, procCount, memoryBlockInst, blockCount);
+    	//select algorithm to allocate processes to memory block partitions then overwrites current memory blocks with 
+    	//memory blocks containing the allocated processes
+    	System.out.println("Allocation methods:\n 1. First Fit\n 2. Next Fit \n 3. Best Fit ");
+    	System.out.println("Select method: ");
+    	int selection = input.nextInt();
+    	switch(selection){
+    		case 1:
+    			memoryBlockInst = firstFit(processInst, procCount, memoryBlockInst, blockCount);
+    			break;
+    		case 2:
+    			memoryBlockInst = nextFit(processInst, procCount, memoryBlockInst, blockCount);
+    			break;
+    		case 3:
+    			memoryBlockInst = bestFit(processInst, procCount, memoryBlockInst, blockCount);
+    			break;
+    		default:
+    			System.out.println("!!!Selection not valid!!!");
+    	}
+    	
+    	
+		//forms final list of partitioned memory by containing allocated memory spaces
+		ArrayList <Process> partition = partition(memoryBlockInst, blockCount);
 		
 		//displays allocated and unallocated memories
 		printPartition(partition);
 		printUnallocatedProcesses(processInst, procCount);
-
+		
 		input.close();
 	}
 
 	public static Process [] processInitializer(int count, Scanner inp){
-		
-    	//initialize process
     	Process [] localProcess = new Process[count];
     	
     	for (int i = 0; i < count; i++){
     		localProcess[i] = new Process();
     		localProcess[i].procID = i+1;
-    		System.out.print("Please enter size for proc["+localProcess[i].procID+"]: ");
+    		System.out.println("Please enter size for proc["+localProcess[i].procID+"]: ");
     		localProcess[i].size = inp.nextInt();
     	}
     	return localProcess;
 	}
 
 	public static MemoryBlock [] memoryBlockInitializer(int count, Scanner inp){
-		//initialize memory blocks
     	MemoryBlock [] localmemoryBlock = new MemoryBlock[count];
 
     	for (int i = 0; i < count; i++){
     		localmemoryBlock[i] = new MemoryBlock();
-    		System.out.print("Please enter size for memBlock["+(i+1)+"]: ");
+    		System.out.println("Please enter size for memBlock["+(i+1)+"]: ");
     		localmemoryBlock[i].size = inp.nextInt();
     		localmemoryBlock[i].processList = new ArrayList<Process>();
     	}
-
     	return localmemoryBlock;
 	}
 
-	public static ArrayList<Process> firstFit(Process [] inpProc, int procNum, MemoryBlock [] inpMemBlock, int blockNum){
+	public static MemoryBlock [] firstFit(Process [] inpProc, int procNum, MemoryBlock [] inpMemBlock, int blockNum){
 		//every process
 		for (int i = 0; i < procNum; i++){
 			//every memory block
@@ -97,7 +113,91 @@ class MemoryManagement{
 				}
 			}
 		}
+		return inpMemBlock;
+	}
 
+	public static MemoryBlock [] nextFit(Process [] inpProc, int procNum, MemoryBlock [] inpMemBlock, int blockNum){
+		/*
+		this counter is needed to keep track of the 
+		index of the MemoryBlock the iteration is currently on
+		*/
+		int counter = 0;
+		
+		//every processes
+		for (int i = 0; i < procNum; i++){
+			/*
+			the loop to iterate through MemoryBlocks starts with the
+			index of where the last process was allocated
+			*/ 
+			for (int j = counter; j < blockNum; j++){
+				//if process is less than memory hole
+				if (inpProc[i].size <= inpMemBlock[j].size){
+					//reduce the size of memory by size of process
+					inpMemBlock[j].size -= inpProc[i].size;
+					
+					//change allocated status
+					inpProc[i].allocatedStatus = true;
+
+					//add the process into available memory block
+					inpMemBlock[j].processList.add(inpProc[i]); 
+					
+					//breaks out of loop of checking every memory block as process has been allocated
+					//and can now move on to the next process
+					break;
+				}
+				//if the current MemoryBlock is full,
+				else {
+					//move on to the next MemoryBlock
+					counter++;
+				}
+			}
+		}
+		return inpMemBlock;
+	}
+
+	public static MemoryBlock [] bestFit(Process [] inpProc, int procNum, MemoryBlock [] inpMemBlock, int blockNum){
+		//to keep track of min available space 
+		int minSize = 0;
+		//keeps track of MemoryBlock with smallest available space
+		int minIndex = 0;
+		int currSize = 0;
+		//for every process
+		for (int i = 0; i < procNum; i++){
+			//reset the min value every time we move on to next process 
+			//so that we can keep track of new minSize
+			minSize = Integer.MAX_VALUE;
+			//every memory block
+			for (int j = 0; j < blockNum; j++){
+				//need to make sure MemoryBlock has	sufficient space			
+				if (inpMemBlock[j].size > inpProc[i].size){
+					currSize = inpMemBlock[j].size - inpProc[i].size;
+					//if there is a smaller available space,  
+					if (currSize < minSize){
+						//store new minimum available space
+						minSize = currSize;
+						//update index of new memoryBlock with smallest available memory
+						minIndex = j;
+					}
+				}
+			}
+			//reduce the size of memory by size of process
+			inpMemBlock[minIndex].size -= inpProc[i].size;
+			
+			//change allocated status
+			inpProc[i].allocatedStatus = true;
+
+			//add the process into available memory block
+			inpMemBlock[minIndex].processList.add(inpProc[i]); 
+		}
+		return inpMemBlock;
+	}
+
+	/*
+	forms final list of partitioned memory by containing allocated memory spaces. This
+	list treats unoccupied MemoryBlocks as Processes so that the type matches so that
+	the unoccupied memory spaces can be added to the list
+	*/
+	public static ArrayList<Process> partition(MemoryBlock [] inpMemBlock, int blockNum){
 		//array list forming final list of partitioned memory
 		ArrayList <Process> localPartition = new ArrayList<Process>();
 		
@@ -123,24 +223,10 @@ class MemoryManagement{
 				localPartition.add(temp);
 			}
 		}
-
-		return localPartition;
-	}
-
-	public static ArrayList<Process> nextFit(Process [] inpProc, int procNum, MemoryBlock [] inpMemBlock, int blockNum){
-		//Implement Next Fit
-		ArrayList <Process> localPartition = new ArrayList<Process>();
-		return localPartition;
-	}
-
-	public static ArrayList<Process> bestFit(Process [] inpProc, int procNum, MemoryBlock [] inpMemBlock, int blockNum){
-		//Implement Best Fit
-		ArrayList <Process> localPartition = new ArrayList<Process>();
 		return localPartition;
 	}
 
 	public static void printPartition(ArrayList <Process> inputList){
-		//Print partitions
 		System.out.println("========================================");
 		System.out.println("  Index  |  procID  |  size  |  status  ");
 		for (int i = 0; i < inputList.size(); i++){
@@ -149,8 +235,7 @@ class MemoryManagement{
 	}
 
 	public static void printUnallocatedProcesses(Process [] inpProc, int count){
-		//print unallocated processes
-		System.out.println("------Unallocated processes---------");
+		System.out.println("--------Unallocated processes---------");
 		for (int i = 0; i < count; i++){
 			if(inpProc[i].allocatedStatus == false){
 				System.out.println("processInst["+inpProc[i].procID+"] = " + inpProc[i].size);
